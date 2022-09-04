b667013de3267fd97f90fac34f36cb76d659479c
m-hari16 <active.hari16@gmail.com>
2022-09-04 22:30:27 +0700

    function with parameter and two return value

--- function/func.go
+++ function/func.go
@@ -10,7 +10,15 @@ func sumNum(number ...int) int32 {
 	return int32(total)
 }
 
+// simple function with parameter and two return value
+func showFullName(fname string, lname string) (string, string){
+	return fname, lname
+}
+
 func main() {
 	pleaseSum := sumNum(3,2,3,4,5,1)
 	println(pleaseSum)
+
+	firstName, _ := showFullName("Moh", "Harianto")
+	println(firstName)
 }
