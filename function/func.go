a9cbe7b7f2eb4bfde2ef0ec002cb863b35329e35
m-hari16 <active.hari16@gmail.com>
2022-09-04 22:31:30 +0700

    function with parameter and named return value

--- function/func.go
+++ function/func.go
@@ -15,10 +15,21 @@ func showFullName(fname string, lname string) (string, string){
 	return fname, lname
 }
 
+// we can use name for return value of function
+func no_type(type_identity string, no_identity string)(tipe, no_tipe string){
+	tipe = type_identity
+	no_tipe = no_identity
+
+	return
+}
+
 func main() {
 	pleaseSum := sumNum(3,2,3,4,5,1)
 	println(pleaseSum)
 
 	firstName, _ := showFullName("Moh", "Harianto")
 	println(firstName)
+
+	tipe_identitas, no_identitas := no_type("ktp", "6736497932430002")
+	println(tipe_identitas, no_identitas)
 }
